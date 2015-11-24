4cbe939ebae6b6579f9e4807b7adc0d7731fb670
Geoffrey Floyd <geoffrey.floyd@hoomanlogic.com>
2015-11-23 20:09:31 -0700

    add SplitLayout to public export

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 module.exports = exports = {
     Layout: require('./src/Layout'),
+    SplitLayout: require('./src/SplitLayout'),
     LayoutMixin: require('./src/LayoutMixin'),
     WindowSizeLayout: require('./src/WindowSizeLayout')
 };
