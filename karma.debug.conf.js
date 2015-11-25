223dc0de078007ea4983ecef8c095aae76e7a893
Geoff Manning <gfm@alsoenergy.com>
2015-11-25 15:13:04 -0700

    single run for testall

--- karma.debug.conf.js
+++ karma.debug.conf.js
@@ -2,5 +2,6 @@
 module.exports = function (config) {
     var sharedConfig = require('./karma.shared.conf.js')(config);
     sharedConfig.browsers = ['PhantomJS', 'Chrome', 'Firefox', 'IE'];
+    sharedConfig.singleRun = false;
     config.set(sharedConfig);
 };
